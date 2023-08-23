92b03b5191e8e10197f89330f7813bdc1bffe071
Victor Lantigua <lantiguav@gmail.com>
2023-08-22 21:34:00 -0400

    feat: moved header and footer to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import Image from 'next/image'
-import { performRequest } from '../lib/datocms';
+import { performRequest } from '../lib/datocms'
 
-import { NavBar } from '@/components/NavBar';
-import { Hero } from '@/components/Hero';
-import { Footer } from '@/components/Footer';
+import { Hero } from '@/components/Hero'
 
 const PAGE_CONTENT_QUERY = `
 query MyQuery {
@@ -14,21 +12,18 @@ query MyQuery {
     subcopy
     title
   }
-}`;
-
+}`
 
 export default async function Home() {
-  const { data: { hero } } = await performRequest({ query: PAGE_CONTENT_QUERY });
+  const {
+    data: { hero },
+  } = await performRequest({ query: PAGE_CONTENT_QUERY })
 
-  console.log({ hero });
+  console.log({ hero })
 
   return (
     <>
-      <NavBar />
-      <main className=''>
-        <Hero {...hero} />
-      </main>
-      <Footer/>
+      <Hero {...hero} />
     </>
   )
 }
