103a4b58a032a218a5667549190f667b54d37e99
Victor Lantigua <lantiguav@gmail.com>
2023-08-26 09:50:13 -0400

    feat: adding support for 3 columns in service section

--- components/ServicesSection/index.tsx
+++ components/ServicesSection/index.tsx
@@ -9,16 +9,27 @@ type ServicesSectionProps = {
   services: ServiceType[]
 }
 
+const gridColumns: {
+  three: string
+  four: string
+} = {
+  three: 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-3',
+  four: 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-4',
+}
+
 export const ServicesSection = ({
   htmlId,
   title,
   services,
 }: ServicesSectionProps) => {
+  const { length } = services
+  const columns: 'three' | 'four' = length % 4 === 0 ? 'four' : 'three'
+
   return (
     <section id={htmlId}>
       <Wrapper className='py-20'>
         <h2 className='text-orange-dark text-5xl font-serif mb-7'>{title}</h2>
-        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6'>
+        <div className={`grid ${gridColumns[columns]} gap-6`}>
           {services.map((service) => (
             <ServiceCard key={service.id} {...service} />
           ))}
